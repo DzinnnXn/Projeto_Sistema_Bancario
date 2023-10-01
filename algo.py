fea76111c9be76670c6c6d058b90af249a08d40e
DzinnnXn <andresilvax444@gmail.com>
2023-10-01 12:09:01 -0300

    up

--- algo.py
+++ algo.py
@@ -1,11 +1,12 @@
 import os
 from classes import *
+
 banco = Banco("Bradesco", 123456789, "Jundiaí - SP")
 cliente = Cliente()
 
 def pausar():
     os.system("pause")
-    
+
 def cls():
     os.system("cls")
 
@@ -16,7 +17,7 @@ def main():
         print("[1] - Cadastrar Cliente")
         print("[2] - Excluir Cliente")
         print("[3] - Depositar")
-        print("[4] - Saquar")
+        print("[4] - Sacar")
         print("[5] - Transferência")
         print("[6] - Visualizar Clientes")
         print("[7] - Sair")
@@ -29,41 +30,65 @@ def main():
                 print("-------------------------")
                 nome = input("Digite o nome do novo cliente: ")
                 rg = input("Digite o rg do novo cliente: ")
-                cliente.adicionar_cliente(nome, rg)
+                cliente_novo = Cliente(nome, rg)
+                cliente_novo.criar_conta()  
+                cliente.adicionar_cliente(cliente_novo)
                 print("Cliente cadastrado com sucesso.")
                 pausar()
-            
+
             case 2:
                 cls()
                 print("-------------------------")
                 print("     EXCLUIR CLIENTE     ")
                 print("-------------------------")
                 cliente.listar_clientes()
-                indice = int(input("Digite o indice no cliente que deseja remover \n>>"))
-                cliente.remover_cliente(indice)
+                indice = int(input("Digite o índice do cliente que deseja remover \n>>"))
+                cliente.remover_cliente(indice - 1)  
                 pausar()
-            
+
             case 3:
                 cls()
                 print("-------------------------")
                 print("         DEPÓSITO        ")
                 print("-------------------------")
+                cliente.listar_clientes()
+                indice = int(input("Digite o índice do cliente para fazer o depósito \n>>"))
+                if 0 <= indice - 1 < len(cliente.clientes):
+                    valor = float(input("Digite o valor do depósito: "))
+                    cliente.clientes[indice - 1].deposito(valor)
+                else:
+                    print("Índice de cliente inválido.")
                 pausar()
-            
+
             case 4:
                 cls()
                 print("-------------------------")
                 print("         SAQUE           ")
                 print("-------------------------")
+                cliente.listar_clientes()
+                indice = int(input("Digite o índice do cliente para fazer o saque \n>>"))
+                if 0 <= indice - 1 < len(cliente.clientes):
+                    valor = float(input("Digite o valor do saque: "))
+                    cliente.clientes[indice - 1].saque(valor)
+                else:
+                    print("Índice de cliente inválido.")
                 pausar()
-            
+
             case 5:
                 cls()
                 print("-------------------------")
-                print("     TRASNFERÊNCIA       ")
+                print("     TRANSFERÊNCIA       ")
                 print("-------------------------")
+                cliente.listar_clientes()
+                indice_origem = int(input("Digite o índice do cliente de origem: "))
+                indice_destino = int(input("Digite o índice do cliente de destino: "))
+                if 0 <= indice_origem - 1 < len(cliente.clientes) and 0 <= indice_destino - 1 < len(cliente.clientes):
+                    valor = float(input("Digite o valor da transferência: "))
+                    cliente.clientes[indice_origem - 1].transferencia(cliente.clientes[indice_destino - 1], valor)
+                else:
+                    print("Índice de cliente inválido.")
                 pausar()
-            
+
             case 6:
                 cls()
                 print("-------------------------")
@@ -71,7 +96,7 @@ def main():
                 print("-------------------------")
                 cliente.listar_clientes()
                 pausar()
-            
+
             case 7:
                 cls()
                 print("Saindo...")
--- classes.py
+++ classes.py
@@ -3,32 +3,71 @@ class Banco:
         self.nome = nome
         self.cnpj = cnpj
         self.endereco = endereco
-        
-    def Deposito():
-        pass
-    
-    def Saque():
-        pass
-    
-    def Transferencia():
-        pass
-    
-class Cliente:
-    def __init__(self):
-        self.clientes = []
+        self.clientes = [] 
 
-    def adicionar_cliente(self, nome, rg):
-        self.clientes.append({"nome": nome, "rg": rg})
+    def adicionar_cliente(self, cliente):
+        self.clientes.append(cliente)
 
     def listar_clientes(self):
         for i, cliente in enumerate(self.clientes):
-            i += 1
-            print(f"Índice: {i}  |  Nome: {cliente['nome']}  |  Rg: {cliente['rg']}")
-            
+            print(f"Índice: {i + 1} | Nome: {cliente.nome} | RG: {cliente.rg}")
+
     def remover_cliente(self, indice):
-        indice -= 1
-        if indice < len(self.clientes):
-            del self.clientes[indice]
-            print("Cliente removido com sucesso")
+        if 0 <= indice < len(self.clientes):
+            cliente_removido = self.clientes.pop(indice)
+            print(f"Cliente {cliente_removido.nome} removido com sucesso.")
         else:
-            print("Não existe esse cliente")
+            print("Índice de cliente inválido.")
+
+class Cliente:
+    def __init__(self, nome, rg):
+        self.nome = nome
+        self.rg = rg
+        self.conta = None  
+
+    def criar_conta(self, saldo_inicial=0.0):
+        self.conta = ContaBancaria(saldo_inicial)
+
+    def deposito(self, valor):
+        if self.conta:
+            self.conta.deposito(valor)
+        else:
+            print("O cliente não possui uma conta bancária.")
+
+    def saque(self, valor):
+        if self.conta:
+            self.conta.saque(valor)
+        else:
+            print("O cliente não possui uma conta bancária.")
+
+    def transferencia(self, outra_conta, valor):
+        if self.conta:
+            self.conta.transferencia(outra_conta, valor)
+        else:
+            print("O cliente não possui uma conta bancária.")
+
+class ContaBancaria:
+    def __init__(self, saldo=0.0):
+        self.saldo = saldo
+
+    def deposito(self, valor):
+        if valor > 0:
+            self.saldo += valor
+            print(f"Depósito de R${valor} realizado com sucesso. Saldo atual: R${self.saldo}")
+        else:
+            print("O valor do depósito deve ser maior que zero.")
+
+    def saque(self, valor):
+        if valor > 0 and valor <= self.saldo:
+            self.saldo -= valor
+            print(f"Saque de R${valor} realizado com sucesso. Saldo atual: R${self.saldo}")
+        else:
+            print("Saldo insuficiente ou valor de saque inválido.")
+
+    def transferencia(self, outra_conta, valor):
+        if valor > 0 and valor <= self.saldo:
+            self.saldo -= valor
+            outra_conta.saldo += valor
+            print(f"Transferência de R${valor} realizada com sucesso.")
+        else:
+            print("Saldo insuficiente ou valor de transferência inválido.")
